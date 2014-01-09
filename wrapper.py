a5b1e85b221b1b4750e98354436777789dcf16f7
Knirta <myknirtak@gmail.com>
2014-01-09 11:33:25 +0200

    #2

--- wrapper.py
+++ wrapper.py
@@ -10,17 +10,43 @@ class ApiDB(object):
 	def choose_collection(self, collection_name):
 		self.collection = self.db[collection_name]
 
-	def insert_data(self, kwargs):
-		self.collection.insert(kwargs)
+	def insert_data(self, data):
+		self.collection.insert(data)
 
-	def get_data(self):
-		return self.collection.find()
+	def get_data(self, query = None, projection = None):
+		cursor = self.collection.find(query, projection)
+		return cursor
+
+	def update_data(self, query=None, data=None):
+		pass
+
+	def remove_data(self, query = None):
+		self.collection.remove(query)
+
+	def check_data(self, query, projection, field, name):
+		for_checking = self.collection.find_one(query, projection)
+		if for_checking[field] == name:
+			return 'true'
+		return 'false'
+
+		
+
+		
+
 
 
 if __name__ == '__main__':
 	apidb=ApiDB()
 	apidb.choose_database('testdb')
 	apidb.choose_collection('unicorns')
-	apidb.insert_data({'name': 'Kate', 'role':'student', 'age':29})
-	for row in apidb.get_data():
-		print row
+	#apidb.insert_data({'name': 'Kate', 'role':'student', 'age':29})
+	#apidb.insert_data({'name': 'Ed', 'role':'programmer', 'age':29})
+	#cursor = apidb.get_data({'name':'Ed'}, {'_id': 0, 'name': 1, 'age': 1} )
+	#for row in cursor:
+	#	print row
+	#apidb.remove_data({'name':'Kate'})
+	result=apidb.check_data({'age':29}, {'name': 1}, 'name', 'Ed') 
+	print result
+
+	
+	
